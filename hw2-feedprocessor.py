597f5b996f47db4f13c753975da51891397a593e
Sean Munson <smunson@gmail.com>
2017-10-09 11:58:55 -0700

    Feed processor part of the assignment

--- hw2-feedprocessor.py
+++ hw2-feedprocessor.py
@@ -1,11 +1,11 @@
 
 ### you will need this function later in the homework
 def stripWordPunctuation(word):
-    return word.strip(".,()<>\"\\'~?!;*:[]-+/&")
+    return word.strip(".,()<>\"\\'~?!;*:[]-+/&—")
 
 print("== part 3 ==")
 ### part 3: fieldType() function
-# In hw3feed.txt, note that there are both posts and comments in this feed. There are also posters.
+# In hw2feed.txt, note that there are both posts and comments in this feed. There are also posters.
 # These lines each start with post:, reply:, and from: respectively.
 # You are probably thinking "it would be really helpful if we had a function that I could
 # use to figure out which type of content a line contains."
@@ -29,63 +29,83 @@ print("== part 4 ==")
 # 
 # Print them as:
 # Total posts: <number>
-# Total comments: <number>
+# Total replies: <number>
 #
 
 posts = 0
-comments = 0
+replies = 0
 
-fname = "hw3feed.txt"
-f = open(fname,'r')
+fname = "hw2feed.txt"
+f = open(fname,'r',encoding='utf-8')
 
 # Fill in your code here.
 
 print("Total posts: %d"%posts)
-print("Total comments: %d"%comments)
+print("Total replies: %d"%replies)
 
 print("== part 5 ==")
 ### part 5: printing users
 
-# Your job is to extract the names form the post: lines and print them out,
-# exactly as it is shown in the screenshot in the PDF file. Hint: if you 
-# want to remove "from:" you can use string slicing operations or the replace method.
-
+# Your job is to extract the names of people who made each post: and print them out,
+# exactly as it is shown in the readme. 
+#
+# Note: only onclude people who made posts, not comments
+# 
+# Hint: if you  want to remove "from:" you can use string slicing operations
+# or the replace method.
+#
 # You may use the fieldType() function but you do not have to. You may also define
 # another function, such as fieldContents(), to help you but that optional. 
-
+#
 # Duplicate names are expected for this part!
 
 fname = "hw2feed.txt"
-f = open(fname,'r')
+f = open(fname,'r',encoding='utf-8')
 #fill in code here
 
 
 print("== part 6 ==")
 ### part 6: counting poster contribution frequency
-# See the instructions in the readme file. They are easier to follow with
-# formatting
+### Your next task in `hw2-feedprocessor.py` is to count how often each user posts or replies to the channel. 
+### You will need to use a dictionary to keep a running count of how many times each user posted or replied
+### (a combined count for each) to the channel:
+###
+### 1) The starter code begins by creating an empty dictionary, called pr_count.
+###   Note that you will need to open the file again, because looping through its lines in Part 5 will
+###   have "consumed" those lines. 
+###
+### 2) Once you’ve extracted a user name (the same thing that you printed out in part 4), think of it as a
+###    key in the dictionary, whose value is the number of posts encountered by that user so far. If the key
+###    is not in the dictionary, set the value to 1. If the key is already in the dictionary, set its value 
+###    to 1 more than its current value. (In the language of the accumulation pattern, the count is our 
+###    accumulator variable for that user.) Hint: You have to handle when you have not seen that user before.
+###    You can do that with either `.get()` or with if/else statements. This is your choice.
+### 
+###	Once your program finishes reading through the file, print how many times each user posted or replied.
+### To do this, create a string of the form: `<name>: <X>`, where <name> is the user’s name, and X is the
+### number of times they posted. (See example in README.md for what it should look like.). Print that string.
 
 pr_count = {}
-f = open(fname,'r')
+f = open(fname,'r',encoding='utf-8')
 
-# read in and count the total number of posts and comments for each user
-
-#fill in code here
-
-
-# print the number of times each user posted
+# read in and count the total number of posts and replies for each user
 
 #fill in code here
 
 
-# part 6 - Just for fun: how many unique posters were there?
-# (note this question is optional - but it's one line of code)
+# print the number of times each user posted and replied
+
+#fill in code here
+
+
+# part 6 - How many unique posters/commenters were there?
+# (hint: it's one line of code now that you have pr_count)
 
 print("== part 7 ==")
 ### part 7: counting word frequency
 # This is similar to post count in part 6 and you might
 # even re-use some of your code. Count the number of
-# times each word appears in all posts, but *not* comments
+# times each word appears in all posts, but *not* replies
 #
 # use the stripWordPunctuation() function to get rid of punctuation.
 # note that it is not perfect so some extra punctuation may remain.
@@ -94,44 +114,47 @@ print("== part 7 ==")
 # I.e., "the" and "The" should be the same word
 
 postWordCount = {}
-f = open(fname,'r')
+f = open(fname,'r',encoding='utf-8')
 
 # read in and count of times each word appeared
 
 #fill in code here
 
 
-# print the number of times each word appeared
+# print the number of times each word appeared, but only if the word
+# appeared at least five times
 
 #fill in code here
 
 
-print "== part 8 =="
-### part 8: counting word frequency in comments and posts
+print("== part 8 ==")
+### part 8: counting word frequency in replies and posts
 # for this part, write a function, wordFreq() that will return
-# the word frequency in either posts or comments as a dictionary
+# the word frequency in either posts or replies as a dictionary
 
 # This function must must take a file name and the field type
-# (either post or comment) as parameters
+# (either post or reply) as parameters
 
 # For example, if I want to get a dictionary of word counts in
-# the posts in hw3feed.txt, I should be able to call:
-# wordFreq('hw3feed.txt','post')
+# the posts in hw2feed.txt, I should be able to call:
+# wordFreq('hw2feed.txt','post')
 
 # You can use your code from part 7 as a starting point, or
-# if you wrote part 7 using a function, you may simply edit it
+# if you wrote part 7 using a function, you may edit it
 # to meet the requirements for this part.
 
 # uncomment and begin editing from the next line:
 #def wordFreq
 
-# to test ,you can uncomment and run these  lines:
-#if wordFreq(fname,'post')["anyone"] == 9 and wordFreq(fname,'post')["eclipse"] == 5:
+# to test ,you can uncomment and run these lines:
+#wfp = wordFreq(fname,'reply')
+#if wfp["environment"] == 1 and wfp["file"] == 3 and wfp["pycharm"] == 1 and wfp.get("post",0) == 0:
 #    print("Looks like wordFreq() works fine for posts")
 #else:
 #    print("We got some errors with wordFreq() for posts.")
-#  
-#if wordFreq(fname,'reply')["file"] == 24 and wordFreq(fname,'reply')["if"] == 39:
+# 
+#wfr = wordFreq(fname,'reply') 
+#if wfr["christina’s"] == 1 and wfr["be"] == 5 and wfr.get("reply",0) == 0:
 #    print("Looks like wordFreq() works fine for replies")
 #else:
 #    print("We got some errors with wordFreq() for replies.")
